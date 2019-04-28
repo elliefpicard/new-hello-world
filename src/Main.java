c70dd2d4706932a4f2f1131553ab37cc9f89d54d
Ellie Picard <elliefpicard@gmail.com>
2019-04-28 13:56:15 -0700

    Created basic hello world output.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * A basic hello world program.
+ *
+ * @author Ellie Picard
+ * @version 2019.4.27
+ */
+
+public class Main {
+    /**
+     * Say hello!
+     */
+    public static void main(String[] args) {System.out.println("Hello world!");
+    }
+}
--- src/NewHelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * A basic hello world program.
- *
- * @author Ellie Picard
- * @version 2019.4.27
- */
-
-public class NewHelloWorld {
-}
