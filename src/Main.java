75faeb5341650dea0c2f474184120c70b6ef42fe
Ellie Picard <elliefpicard@gmail.com>
2019-04-28 15:15:47 -0700

    More code changes.

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,12 @@ public class Main {
      * Say hello!
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");}
-
+        System.out.println("Hello world!");
+        System.out.println("Hello world, again!");}
 
     /* The source code has been modified and commented,
     a second commit will be made and pushed to GitHub from local.
      */
 
-    
+
 }
