5c53ff9ec65a5ff7536e9d7186cd591e61461c77
Ellie Picard <elliefpicard@gmail.com>
2019-04-28 14:50:27 -0700

    Added comments to source code for project.

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@ public class Main {
     /**
      * Say hello!
      */
-    public static void main(String[] args) {System.out.println("Hello world!");
-    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");}
+
+
+    /* The source code has been modified and commented,
+    a second commit will be made and pushed to GitHub from local.
+     */
 }
